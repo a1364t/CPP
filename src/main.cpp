5ba513f05125abe5b6fbb4c51cafeb0f9be376db
Alireza <ali1985reza@gmail.com>
2022-11-10 18:32:59 +1100

    read write file

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include "include/list.h"
+#include "include/database.h"
 
 int main(int arg_count, char *args[]) {
+    List simplelist;
+    Database data;
+
     if ( arg_count > 1 ) {
-        List simplelist;
         simplelist.name = string(args[1]);
         simplelist.print_menu();
+        data.write(simplelist.list);
+        data.read();
     }
     else {
         cout << "User name not supplied ... Exiting the program" << endl;
     }
+
     return 0;
 }
+
 
